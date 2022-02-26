15f6315493b5351b36d21564014e25f6c0b51a56
Lamar Daughma <lite5h4dow@gmail.com>
2022-02-26 11:15:16 +0000

    adding local changes

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from tinydb import TinyDB,Query
 # import json
 # import os
 import uuid
-# import time
-# import board
-# import neopixel
-# import math
+import time
+import board
+import neopixel
+import math
 
 #importing config file and defaulting to None if file isnt there
 # config = None
@@ -17,16 +17,17 @@ import uuid
 # 
 app = Flask(__name__, static_url_path="", static_folder="static", template_folder="templates")
 app.debug = True
-# pixel_pin = board.D18
+pixel_pin = board.D18
 
 d = TinyDB("./db.json")
-num_pixels = 144
-group_size = d.all().count
-tail = 6
-# Order = neopixel.RGB
-# pixels = neopixel.NeoPixel(
-#             pixel_pin, num_pixels, brightness=0.1, auto_write=False, pixel_order=Order
-#         )
+num_pixels = 416 * 6
+group_size = 3
+group_pixels = 13
+tail = 3 
+Order = neopixel.RGB
+pixels = neopixel.NeoPixel(
+            pixel_pin, num_pixels, brightness=1, auto_write=False, pixel_order=Order
+        )
 
 
 ''' Utility function that puts all 
@@ -44,7 +45,6 @@ def segregate(arr, size):
 ''' Find the smallest positive missing number 
 in an array that contains all positive integers '''
 def findMissingPositive(arr, size):
-      
     # Mark arr[i] as visited by 
     # making arr[arr[i] - 1] negative. 
     # Note that 1 is subtracted 
@@ -79,37 +79,62 @@ def findUnused():
     return findMissing(positions, positions.__len__())
 
 #returns the ammount of pixels are available to be allocated to groups
-# def usable_pixels():
-#     return int(num_pixels - (num_pixels % group_size))
+def usable_pixels():
+    return int(num_pixels - (num_pixels % group_size))
 
 #returns the maximum ammount of usable groups in the provided pixel count
-# def max_groups():
-#     return math.floor(usable_pixels() / group_size)
+def max_groups():
+    return math.floor(usable_pixels() / group_size)
 
 #returns the group that the current led is in
-# def current_group(pos):
-#     return math.floor(pos/group_size)
+def current_group(pos):
+    return math.floor(pos/group_size)
 
 #returns position relative to the brightest group
-# def wrap_calc(pos, brightest):
-#     group = current_group(pos)
-#     sigma = brightest - group
-#     return sigma if sigma >= 0 else sigma + max_groups()
+def wrap_calc(pos, brightest):
+    group = current_group(pos)
+    sigma = brightest - group
+    return sigma if sigma >= 0 else sigma + max_groups()
 
 # run scan animation
-# def scan_to(scan_to):
-#     for group in range (math.floor(scan_to / group_size)):
-#         for position in range(scan_to if scan_to <= usable_pixels() else usable_pixels()):
-#             usable_tail = wrap_calc(position, group) +1
-#             if usable_tail <= tail:
-#                 r = g = b = math.ceil(255 / usable_tail)
-#                 pixels[position] = (r,g,b)
-#             else:
-#                 pixels[position] = (0,0,0)
-#         pixels.show()
-#         time.sleep(0.001)
-# 
+def scan_to(scan_to):
+    g = current_group(scan_to)
+    tail_end = g-tail
 
+    for group in range(g+1):
+        a = [(0,0,0)] * ((g-tail)*group_pixels) if (g-tail) < 0 else []
+        b = []
+        for i in range(tail-g):
+            rc = (255/(tail+1) * i)
+            gc = (255/(tail+1) * i)
+            bc = (255/(tail+1) * i)
+            t = [(rc,gc,bc)] * group_pixels
+            b = b + t
+
+        c = [(255,255,255)] * group_pixels
+        inter = a+b+c
+        remainder = [(0,0,0)] * (num_pixels - len(inter))
+
+        whole = inter+remainder
+        for i, val in enumerate(whole):
+            pixels[i] = val
+
+        print(whole)
+        time.sleep(0.001)
+        pixels.show()
+
+def light_position(position):
+    before = [(0,0,0)] * (current_group(position) * group_pixels)
+    g = [(255,255,255)] * group_pixels
+    splat = before + g
+    after = [(0,0,0)] * (num_pixels - len(splat))
+    result = splat + after
+    
+    print(result)
+
+    for i, val in enumerate(result):
+        pixels[i] = val
+    pixels.show()
 
 def byPosition(phone):
     return phone['position']
@@ -146,25 +171,16 @@ def editPhone():
 
     return "updatedPhone", 200
     
-@app.post("/deletePhone")
-def deletePhone():
-    if request.json == None:
-        return "Invalid Request", 400
-
-    d.remove(phone.uuid == request.json["uuid"])
+@app.post("/deletePhone/<uuid>")
+def deletePhone(uuid):
+    d.remove(phone.uuid == uuid)
     return "Deleted", 200
 
-# @app.put("/scanTo")
-# def scanTo():
-#     if request.json == None:
-#         return "invalid request", 400
-#     
-#     if "position" not in request.json:
-#         return "position missing", 400
-# 
-#     print("would scan")
-#     scan_to(request.json.position)
-#     return 200
+@app.post("/scanTo/<position>")
+def scanTo(position):
+    print("would scan")
+    scan_to(int(position))
+    return "",200
 
 @app.get("/")
 def index():
@@ -188,6 +204,11 @@ def addPhone():
         phones = []
 
     return render_template("index.html", phones=phones)
+
+@app.post("/lightPosition/<position>")
+def lightPosition(position):
+    light_position(int(position))
+    return "", 200
 
 
 
